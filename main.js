39e0d4244072ef101b47a3510a684d37ef886564
jacksonbloom0518 <jackson.bloom@ufl.edu>
2023-05-31 17:30:35 -0500

    Finally finished r, p, s PROJECT!!!
    stuff is very challenging but just need to keep going
    back and drilling and eventually I will get it
    
    don't give up you got all the time in the world

--- main.js
+++ main.js
@@ -2,75 +2,82 @@
 const options = ["rock", "paper", "scissors"]
 
 
-//get computer selection //
+// get computer choice //
 
-function getComputerSelection() {
-    choice = options[(Math.floor(Math.random() * options.length))];
+function getComputerChoice() {
+    const choice = options[(Math.floor(Math.random() * options.length))];
     return choice
 }
 
+// get winner arguments //
 
-//pickWinner //
-
-function getWinner (playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "tie"
-    } else if(
-         (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "scissors" && computerSelection == "paper") ){
+function getWinner(playerSelection, computerSelection) {
+    if
+        (playerSelection == computerSelection) {
+            return "tie"
+        } else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ){
         return "player"
-    } else {
-        return "computer"
-    }
-
-}
-
-//playRound //
-
+        } else {
+            return "computer"
+        }}
+// playRound //
 function playRound (playerSelection, computerSelection) {
-    const result =  getWinner(playerSelection, computerSelection);
+    const result = getWinner(playerSelection, computerSelection);
     if (result == "tie") {
         return `It's a tie`
-    } else if
-        (result == "player") {
-            return `You won! ${playerSelection} beats ${computerSelection}!`
-        } else {
-            return `You lost! ${computerSelection} beats ${playerSelection}!`
-        }
+    } else if (result == "player") {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    }
+ 
     
 }
 
-//test
-
-
-// play five rounds //
+// play five rounds//
 
 function game() {
-for (i = 0; i < 5; i++) {
-const playerSelection = playerPrompt();
-const computerSelection = getComputerSelection();
-console.log(playRound(playerSelection, computerSelection));
-}
- }
+    let playerScore = 0;
+    let computerScore = 0;
+    for (i = 0; i < 5; i++) {
 
-// make a prompt for player selection //
-function playerPrompt() {
-    let question = prompt ("rock, paper, or scissors?")
-    if (question.toLowerCase() == "rock"){
-        return playerSelection = "rock"
-    } else if (question.toLowerCase() == "paper"){
-        return playerSelection = "paper"
-    } else  {
-            return "scissors"
+        const playerSelection = playerPrompt();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+// Keep Score //
+        if(getWinner(playerSelection, computerSelection) == "player"){
+            playerScore++;
+        } else if(getWinner(playerSelection,computerSelection) == "computer"){
+        computerScore++;   
+        } else if(getWinner(playerSelection, computerSelection) == "tie"){
+            playerScore
+        }
+ console.log(`${playerScore}-${computerScore}`)
     }
 
+//printing out a message based on score result //
+  if (playerScore > computerScore){
+     console.log("congradulations")
+  }  else if (playerScore < computerScore){
+    console.log("DAMMM YOU LOST TO A COMPUTER")
+  } else {
+    return console.log("HOW DO YOU TIE IN A BEST OF 5")
+  }
+ 
 }
 
-game()
+// player Choice Prompt //
 
-
-
-
-
-
+function playerPrompt() {
+    let question = prompt(`rock, paper, scissors?`)
+    if (question.toLowerCase() == "rock"){
+        return playerSelection = "rock";
+    } else if(question.toLowerCase() == "paper"){
+        return playerSelection="paper";
+    } else {
+        return "scissors"
+    } 
+} game ()
